8681131937ca18c6139e1e0628db0764dc2b12b4
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-11-19 17:00:06 +0000

    Updated preprocess script to be reusable

--- CSC_8631_Analysis/munge/01-A.R
+++ CSC_8631_Analysis/munge/01-A.R
@@ -3,19 +3,19 @@
 library(dplyr)
 library(readr)
 
-# Combines the Enrolments into one Data Frame
-df_Enrolments <- list.files(path="C:/Users/Harvey/Documents/Newcastle University/CSC_8631_Project/CSC_8631_Analysis/data/Enrolments", full.names = TRUE) %>% 
+# Combines the Enrollments into one Data Frame
+df_Enrolments <- list.files(path="~/Documents/CSC_8631_Project/CSC_8631_Analysis/data/Enrolments", full.names = TRUE) %>% 
   lapply(read_csv) %>% 
   bind_rows
 
 # Combines the Question Response into one Data Frame
 # Question response will require additional analysis as each individual run has their own stats
-df_question_response <- list.files(path="C:/Users/Harvey/Documents/Newcastle University/CSC_8631_Project/CSC_8631_Analysis/data/Question Response", full.names = TRUE) %>% 
+df_question_response <- list.files(path="~/Documents/CSC_8631_Project/CSC_8631_Analysis/data/Question Response", full.names = TRUE) %>% 
   lapply(read_csv) %>% 
   bind_rows 
 
 # Combines the Video Stats into one Data Frame
 # Video stats will require additional analysis as each individual run has their own stats
-df_video_states <- list.files(path="C:/Users/Harvey/Documents/Newcastle University/CSC_8631_Project/CSC_8631_Analysis/data/Video Stats", full.names = TRUE) %>% 
+df_video_states <- list.files(path="~/Documents/CSC_8631_Project/CSC_8631_Analysis/data/Video Stats", full.names = TRUE) %>% 
   lapply(read_csv) %>% 
   bind_rows 
