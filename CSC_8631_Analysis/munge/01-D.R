90fff27861851d619aab3156350a938c59b7596f
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-11-25 10:58:41 +0000

    Preprocess for Question Response DF

---
+++ CSC_8631_Analysis/munge/01-D.R
@@ -0,0 +1,6 @@
+#Filter data based on week number to isolate question responses in df_question_response
+library(dplyr)
+
+df_qr_w1= filter(df_question_response, week==1)
+df_qr_w2= filter(df_question_response, week==2)
+df_qr_w3= filter(df_question_response, week==3)
