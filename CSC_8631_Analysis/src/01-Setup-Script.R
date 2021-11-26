5bde960fb40a74d007a33adb02494266fec3fb8e
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-11-26 15:23:21 +0000

    Comments added for instructions

--- CSC_8631_Analysis/src/01-Setup-Script.R
+++ CSC_8631_Analysis/src/01-Setup-Script.R
@@ -1,3 +1,6 @@
+#Setup script for Project
+#Please highlight line 4, 5 & 6 and press "Run" button on top right or press "Source"
+
 setwd("~/Documents/CSC_8631_Project/CSC_8631_Analysis")
 library(ProjectTemplate)
 load.project()
