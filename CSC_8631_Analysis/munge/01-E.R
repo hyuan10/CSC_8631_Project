9ca69bd35b25169376ddc82ad4ef6b7898f9c1d5
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-11-30 14:24:55 +0000

    Added sum of question response table

--- CSC_8631_Analysis/munge/01-E.R
+++ CSC_8631_Analysis/munge/01-E.R
@@ -3,12 +3,30 @@ sum_qr_w1 <- sum(df_qr_w1$week == 1)
 sum_qr_w2 <- sum(df_qr_w2$week == 2)
 sum_qr_w3 <- sum(df_qr_w3$week == 3)
 
-# Sums the number of results in the column which is FALSE for each week
+# Sums the number of results in the column which is FALSE and True for each week
 sum_qr_w1f <- sum(df_qr_w1$result == FALSE)
 sum_qr_w2f <- sum(df_qr_w2$result == FALSE)
 sum_qr_w3f <- sum(df_qr_w3$result == FALSE)
+sum_qr_w1t <- sum(df_qr_w1$result == TRUE)
+sum_qr_w2t <- sum(df_qr_w2$result == TRUE)
+sum_qr_w3t <- sum(df_qr_w3$result == TRUE)
 
+# Total Correct and Incoorect as a Percentage
+sum_w1_f = (sum_qr_w1t/sum_qr_w1)*100
+sum_w2_f = (sum_qr_w2t/sum_qr_w2)*100
+sum_w3_f = (sum_qr_w3t/sum_qr_w3)*100
+sum_w1_t = (sum_qr_w1f/sum_qr_w1)*100
+sum_w2_t = (sum_qr_w2f/sum_qr_w2)*100
+sum_w3_t = (sum_qr_w3f/sum_qr_w3)*100
 
+# Sum QR as data Frame
+qr_table = data.frame(Total_Responses = c(sum_qr_w1,sum_qr_w2,sum_qr_w3), 
+                      Total_Correct = c(sum_qr_w1t,sum_qr_w2t,sum_qr_w3t), 
+                      Total_Correct_Percentage = c(sum_w1_t,sum_w2_t,sum_w3_t),
+                      Total_Incorrect = c(sum_qr_w1f,sum_qr_w2f,sum_qr_w3f), 
+                      Total_Incorrect_Percentage = c(sum_w1_f,sum_w2_f,sum_w3_f))
+                        
+                        
 # Sum the total number of students
 Total_no_students <- length(df_Enrolments$learner_id)
 
