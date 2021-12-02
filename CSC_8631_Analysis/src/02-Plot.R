d3013e840968b21096189b2e4e73ec5b34e8700e
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-12-02 04:38:02 +0000

    Added plots for frequency of incorrect results

--- CSC_8631_Analysis/src/02-Plot.R
+++ CSC_8631_Analysis/src/02-Plot.R
@@ -6,18 +6,19 @@ library(ggplot2)
 Colours <-c("Week 1" = "dark green", "Week 2" = "orange", "Week 3" = "light blue")
 
 
-P <- ggplot() +
-  ggtitle("Percentage of Views at Video Progress vs Video Progress") +
-  labs(x="Video Progress (%)", y= "Percentage of Views at Video Progress", colour="Legend")+
-  xlim(0, 100)+ ylim(50, 80)+
-  geom_point(data=colMeans_vs_w1, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 1"), shape =16) +
-  geom_point(data=colMeans_vs_w2, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 2"), shape =17) +
-  geom_point(data=colMeans_vs_w3, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 3"), shape =18) +
-  geom_smooth(data=colMeans_vs_w1,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 1"), formula = y ~ x, method ="lm", se=F) +
-  geom_smooth(data=colMeans_vs_w2,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 2"), formula = y ~ x, method ="lm", se=F)+
-  geom_smooth(data=colMeans_vs_w3,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 3"), formula = y ~ x, method ="lm", se=F)+
-  theme_bw()+
-  scale_color_manual(values = Colours)
+P <- 
+  ggplot() +
+    ggtitle("Percentage of Views at Video Progress vs Video Progress") +
+    labs(x="Video Progress (%)", y= "Percentage of Views at Video Progress", colour="Legend")+
+    xlim(0, 100)+ ylim(50, 80)+
+    geom_point(data=colMeans_vs_w1, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 1"), shape =16) +
+    geom_point(data=colMeans_vs_w2, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 2"), shape =17) +
+    geom_point(data=colMeans_vs_w3, aes(x=Percentage, y=View_Percentage_Mean, colour="Week 3"), shape =18) +
+    geom_smooth(data=colMeans_vs_w1,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 1"), formula = y ~ x, method ="lm", se=F) +
+    geom_smooth(data=colMeans_vs_w2,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 2"), formula = y ~ x, method ="lm", se=F)+
+    geom_smooth(data=colMeans_vs_w3,aes(x=Percentage, y=View_Percentage_Mean, colour="Week 3"), formula = y ~ x, method ="lm", se=F)+
+    theme_bw()+
+    scale_color_manual(values = Colours)
 
 
 P
@@ -25,7 +26,8 @@ P
 # Plot of Correct and Incorrect answers by Percentage against Week
 
 
-QR <- ggplot(data=qr_table_percent, aes(x=Week, y=Result_Percent, fill=Result)) +
+QR <- 
+  ggplot(data=qr_table_percent, aes(x=Week, y=Result_Percent, fill=Result)) +
       geom_col()+
       theme_bw()+
       scale_fill_manual(values = c("#34eb6b", "#eb3446"))+
@@ -34,3 +36,33 @@ QR <- ggplot(data=qr_table_percent, aes(x=Week, y=Result_Percent, fill=Result)) 
 
 
 QR    
+
+
+# Plot of frequency of incorrect results
+w1res_plot <-
+  ggplot(data=w1res, aes(x=Num_Incorrect, y=Freq)) +
+    ggtitle("Frequency of Incorrect Results Week 1") +
+    labs(x="Number of Incorrect Results", y= "Frequency")+
+    geom_bar(stat="identity", fill="#08588D")+
+    theme_bw()
+
+w1res_plot
+
+w2res_plot <-
+  ggplot(data=w2res, aes(x=Num_Incorrect, y=Freq)) +
+  ggtitle("Frequency of Incorrect Results Week 2") +
+  labs(x="Number of Incorrect Results", y= "Frequency")+
+  geom_bar(stat="identity", fill="#08588D")+
+  theme_bw()
+
+w2res_plot
+
+w3res_plot <-
+  ggplot(data=w3res, aes(x=Num_Incorrect, y=Freq)) +
+  ggtitle("Frequency of Incorrect Results Week 3") +
+  labs(x="Number of Incorrect Results", y= "Frequency")+
+  geom_bar(stat="identity", fill="#08588D")+
+  theme_bw()
+
+w3res_plot
+
