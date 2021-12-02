c83f4c99aec30ddc29f4fb06ac77eaaedbd556a8
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-12-02 17:08:22 +0000

    Update to 03 Script for printing

--- CSC_8631_Analysis/src/03-Incorrect_Values.R
+++ CSC_8631_Analysis/src/03-Incorrect_Values.R
@@ -3,16 +3,16 @@ Week1F = function(){
   return(sort(-table(df_qr_w1$learner_id))[1:5])
 }
 
-Week1F()*-1
+print(Week1F()*-1)
 
 Week2F = function(){
   return(sort(-table(df_qr_w2$learner_id))[1:5])
 }
 
-Week2F()*-1
+print(Week2F()*-1)
 
 Week3F = function(){
   return(sort(-table(df_qr_w3$learner_id))[1:5])
 }
 
-Week3F()*-1
+print(Week3F()*-1)
