94384220bbc3afbc909828c8819190042ca67d66
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-12-03 12:37:17 +0000

    Reformatted script for bettwe viewing

--- CSC_8631_Analysis/src/03-Incorrect_Values.R
+++ CSC_8631_Analysis/src/03-Incorrect_Values.R
@@ -1,18 +1,22 @@
 # Top 5 students with incorrect inputs in each week
 Week1F = function(){
-  return(sort(-table(df_qr_w1$learner_id))[1:5])
-}
+      return(sort(-table(df_qr_w1$learner_id))[1:5])
+    }
 
-print(Week1F()*-1)
+  print(Week1F()*-1)
+
+
 
 Week2F = function(){
-  return(sort(-table(df_qr_w2$learner_id))[1:5])
-}
+      return(sort(-table(df_qr_w2$learner_id))[1:5])
+    }
 
-print(Week2F()*-1)
+  print(Week2F()*-1)
+
+
 
 Week3F = function(){
-  return(sort(-table(df_qr_w3$learner_id))[1:5])
-}
+      return(sort(-table(df_qr_w3$learner_id))[1:5])
+    }
 
-print(Week3F()*-1)
+  print(Week3F()*-1)
